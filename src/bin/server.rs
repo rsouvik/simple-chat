6635d0b43b00ecc0adfb81fda03e30460f256c99
rsouvik <sales@getmypickly.com>
2025-03-14 17:32:48 -0700

    updating with appropriate flags for flamegraph

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -80,13 +80,16 @@ async fn handle_connection(
                                                 users.insert(addr, (fe,se+1));
                                             }
                                         }*/
-                                        if let Some(fv) = mut users.get(&addr).map(|t| &t.0){
+                                        if let Some(fv) = users.get(&addr).cloned().map(|t| &t.0){
                                             let sname = fv;
-                                            if let Some(sv) = users.get(&addr).map(|t| &t.1){
+                                            if let Some(sv) = users.get(&addr).cloned().map(|t| &t.1){
                                                 let count = sv;
-                                                users.insert(addr, (String::from(sname),count+1));
+                                                users.insert(addr, (sname.clone(),count+1));
                                             }
                                         }
+                                        /*users.entry(addr).
+                                             .and_modify(|entry| entry.1+=1)
+                                             .or_insert("default_name".to_string(),1);*/
                                         //users.insert(addr, ((users.get(&addr)).as_ref().0,(users.get(&addr)).as_ref().1+1));
                                     }
                                     else {
