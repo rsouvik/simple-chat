963903f8ebeb8184d010523c452df80de1b1d887
rsouvik <sales@getmypickly.com>
2025-04-10 13:03:39 -0700

    feat: adding a web-server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -11,7 +11,7 @@ use actix_web::{web, App, HttpServer, Responder};
 extern crate actix_web;
 
 //mod webutils;
-use crate::webutils::{index, indexPost};
+use crate::chat_app::webutils::{index, indexPost};
 
 // Structure to hold user data
 struct User {
