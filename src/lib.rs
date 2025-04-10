4e85868e40f8a51583c566b850453cfda1cc5c60
rsouvik <sales@getmypickly.com>
2025-04-10 15:10:15 -0700

    feat: adding a web-server

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,9 @@ use diesel::prelude::*;
 use dotenvy::dotenv;
 use std::env;
 
-pub mod models;
-pub mod schema;
-pub mod webutils;
+pub(crate) mod models;
+pub(crate) mod schema;
+pub(crate) mod webutils;
 
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
--- src/webutils.rs
+++ src/webutils.rs
@@ -9,7 +9,7 @@ struct MyQueryParams {
 }
 
 //get handler
-pub(super) async fn index(query: web::Query<MyQueryParams>) -> impl Responder {
+pub(crate) async fn index(query: web::Query<MyQueryParams>) -> impl Responder {
     let name = &query.user_name;
 
     HttpResponse::Ok().body(format!("Data sent to libp2p swarm: {}", name))
