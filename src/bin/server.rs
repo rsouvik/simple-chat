5bee3bd8732ef18afa631e5ff736b1b167199f1f
rsouvik <sales@getmypickly.com>
2025-04-10 14:10:04 -0700

    feat: adding a web-server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -11,8 +11,8 @@ use std::sync::Arc;
 use actix_web::{web, App, HttpServer, Responder};
 extern crate actix_web;
 
-mod webutils;
-use webutils::{index, indexPost};
+//mod webutils;
+use crate::webutils::{index, indexPost};
 
 // Structure to hold user data
 struct User {
