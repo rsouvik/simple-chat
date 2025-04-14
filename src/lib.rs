029302100fc3a7b039e0425e95c50b89a52765d9
rsouvik <sales@getmypickly.com>
2025-04-14 12:23:22 -0700

    feat: adding a web-server

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::env;
 
 pub(crate) mod models;
 pub(crate) mod schema;
-pub(crate) mod webutils;
+pub mod webutils;
 
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
--- src/webutils.rs
+++ src/webutils.rs
@@ -1,4 +1,4 @@
-pub mod webutils {
+
     use actix_web::{web, Responder, HttpResponse};
     //use crate::SwarmWebMessage;
     use tokio::{sync::mpsc};
@@ -65,4 +65,3 @@ pub mod webutils {
         HttpResponse::InternalServerError().body("Failed to send data to libp2p swarm")
     }*/
     }
-}
