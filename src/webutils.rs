7cd44ad4032465a56b4e4608f595f7b69e8fb478
rsouvik <sales@getmypickly.com>
2025-05-01 18:56:59 -0700

    fix: updating utils to include common functions and ds

--- src/webutils.rs
+++ src/webutils.rs
@@ -63,6 +63,9 @@ impl ServerState {
 pub async fn statsall(query: web::Query<MyQueryParams>, state: web::Data<Arc<ServerState>>) -> impl Responder {
     let users_map = state.users.lock().await; // be careful with unwrap
     let count = users_map.len();
+    for (sock_add, U) in users_map.into_iter() {
+        println!("{} / {}", U.username, U.lifetime_cnt);
+    }
     HttpResponse::Ok().body(format!("Total users: {}", count))
     //HttpResponse::Ok().body(format!("Data sent to libp2p swarm: {}", users.keys().len()))
 }
