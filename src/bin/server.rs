9b9fef5d6406973342d41b25ef60f741793ae149
rsouvik <sales@getmypickly.com>
2025-05-01 19:43:27 -0700

    fix: updating utils to include common functions and ds

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -141,6 +141,8 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
 
     loop {
         let (socket, addr) = listener.accept().await?;
+        println!("socket: {}", socket);
+        println!("address: {}", addr);
         let conn_state = state.clone();
 
         tokio::spawn(async move {
